9882277e718e65c863713b48c7a5dff6129ecf20
kamajiharini608-glitch <kamajiharini608@gmail.com>
2026-06-08 21:53:02 +0530

    Add script.js for LMS functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Welcome to the Learning Management System!");
+}
+
+function enrollCourse(courseName) {
+    alert("Successfully enrolled in " + courseName);
+}
+
+console.log("LMS Project Loaded Successfully");
